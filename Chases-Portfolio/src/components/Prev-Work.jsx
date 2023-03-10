196c9a71a880095eb906c19e44b3eb7535dc1e1a
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2023-03-10 07:54:03 -0500

    Deleted old project. Starting Over

--- Chases-Portfolio/src/components/Prev-Work.jsx
+++
@@ -1,8 +0,0 @@
-
-const Prev = () => {
-  return (
-    <div>Prev-Work</div>
-  )
-}
-
-export default Prev
--- Chases-Portfolio/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Hero from "./Hero";
-import Prev from "./Prev-Work";
-import Contact from "./Contact";
-
-export {
-    Hero,
-    Prev,
-    Contact
-};
