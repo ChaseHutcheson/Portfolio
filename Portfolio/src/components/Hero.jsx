b482deef1c8d6dba11e5184c11bcf4a60cda760d
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2023-03-10 08:39:16 -0500

    Ill fix it later

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <section className='flex flex-row sm:flex-col text-white items-center justify-between w-full h-[100vh] bg-gradient-to-tr from-red-600 to-yellow-500 pt-20 pb-6'>
+    <section className='flex flex-row w-full h-[100vh] sm:flex-col text-white items-center justify-between bg-gradient-to-tr from-red-600 to-yellow-500 pt-20 pb-6'>
         <div className='flex-1'>
             <h1 className=' font-montserrat font-bold text-4xl md:text-5xl'>Hi, Im Chase.</h1>
         </div>
