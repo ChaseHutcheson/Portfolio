15d8bb8c69eb5e1db7cc647c2b2e067c4f782583
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2023-03-10 11:52:44 -0500

    Hero section more or less finished

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Image from '../assets/image.jpg'
 
 function Hero() {
   return (
-    <section className='flex flex-row md:flex-col text-white items-start justify-center bg-gradient-to-tr from-red-600 to-yellow-500 w-full overflow-hidden px-5 sm:px-20'>
-        <div className='flex-1 flex justify-center items-start flex-col'>
-          <div className='flex-1 items-start mt-48'>
-            <h1 className='font-montserrat font-semibold text-5xl mb-1'>Name</h1>
-            <p className='font-montserrat font-normal text-xl text-stone-200'>Software Engineer & Web Developer</p>
+    <section className='flex flex-row md:flex-col text-white items-start justify-center bg-gradient-to-tr from-red-600 to-yellow-500 w-full overflow-hidden px-5 sm:px-20 border-b-4 border-stone-300'>
+        <div className='flex-1 flex justify-center items-start flex-col lg:flex-row md:items-center'>
+          <div className='flex-1 items-start my-3 md:my-28 px-12'>
+            <h1 className='font-montserrat font-bold text-5xl mb-1 -ml-0.5'>Hi, Im Chase.</h1>
+            <p className='font-montserrat font-semibold text-xl text-stone-200 mt-1 mb-2 underline underline-offset-2'>Software Engineer & Web Developer</p>
+            <p className=''>I'm a student at The Mahoning County Carrer & Technical Center taking classes in Software Development and Web Development. I'm an Intermediate Python Coder and Web Developer. Beginner in Databases</p>
           </div>
+          <img src={Image} className="rounded-3xl my-2 md:my-3"/>
         </div>
     </section>
   )
